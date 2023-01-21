f718f875958825f7884a331442040d4805239a75
barjiz <barjizm@gmail.com>
2023-01-21 22:30:48 +0530

    fixed bug

--- utls/colors.tsx
+++ utls/colors.tsx
@@ -8,6 +8,7 @@ export const GREY_COLOR = "#566573"
 export const LIGHT_GREY_COLOR = "#F2F3F4"
 
 
+
 export const PRIMARY_SHADOW = "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px"
 
 
