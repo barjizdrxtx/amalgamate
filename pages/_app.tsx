f7f9a9b1c3385f6eb10e5052431bbf9319a3d924
barjiz <barjizm@gmail.com>
2023-01-25 11:59:03 +0530

    new data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { Layout } from '../components/UI/Layout/Layout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ProtectedRoutes } from '../components/ProtectedRoutes/ProtectedRoutes'
+import Head from 'next/head'
 
 const queryClient = new QueryClient()
 
@@ -13,6 +14,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
 
     <QueryClientProvider client={queryClient}>
+
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+
 
       <ProtectedRoutes>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const index = () => {
 
         <Grid>
 
+          
             <Box sx={{ m: 2 }}>
 
                 <Request />
